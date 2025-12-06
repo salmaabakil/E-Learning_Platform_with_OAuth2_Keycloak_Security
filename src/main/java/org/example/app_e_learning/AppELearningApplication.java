5cf14edf0b926e4b8148e23d13036425b7171375
salmaabakil <153354773+salmaabakil@users.noreply.github.com>
2025-12-06 13:17:21 +0100

    main

---
+++ src/main/java/org/example/app_e_learning/AppELearningApplication.java
@@ -0,0 +1,13 @@
+package org.example.app_e_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppELearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppELearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/app_e_learning/AppELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.app_e_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppELearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
